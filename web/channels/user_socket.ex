36d10943c4dd1648ba1074798b919cf2c04f427c
Marius Vach <rasmus_oo@hotmail.de>
2016-10-24 20:34:09 +0200

    made the app heroku ready

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -5,7 +5,8 @@ defmodule Bbb.UserSocket do
   # channel "room:*", Bbb.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+  timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
